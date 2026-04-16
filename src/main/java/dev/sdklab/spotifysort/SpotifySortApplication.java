efdb1f24b2b5fb806f275b27bbebc48aef8c5515
dk-stars <dstarostin09@gmail.com>
2026-04-16 20:54:08 +0200

    init commit

---
+++ src/main/java/dev/sdklab/spotifysort/SpotifySortApplication.java
@@ -0,0 +1,13 @@
+package dev.sdklab.spotifysort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifySortApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpotifySortApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/sdklab/spotifysort/SpotifySortApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sdklab.spotifysort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifySortApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
